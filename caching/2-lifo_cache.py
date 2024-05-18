78f99e797ace526826115132b7bb23d9f6a55f5c
Fabrice <f.nkurunziz@alustudent.com>
2024-05-18 09:47:25 +0200

    caching

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -28,3 +28,26 @@ class LIFOCache(BaseCaching):
             It assigns to the dictionary self.cache_data
             the item value for the key key
         """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key = self.order.pop()
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+    def get(self, key):
+        """
+            Return the value in self.cache_data linked to key.
+            If key is None or if the key does not exist in
+            self.cache_data, return None.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
