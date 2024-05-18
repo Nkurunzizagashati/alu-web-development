638321189768a163286daa114e2f4cb846dcb6a5
Fabrice <f.nkurunziz@alustudent.com>
2024-05-18 09:44:52 +0200

    adde the put method in the LIFOCache class to put items into the cache

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -13,3 +13,18 @@ class LIFOCache(BaseCaching):
         class LIFOCache that inherits BaseCaching
         class and implements hte LIFO caching system
     """
+
+    def __init__(self):
+        """
+            an init method to inherit all the methods
+            from the class this LIFOCache class is inheriting
+            from
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            put method to put item into the cache.
+            It assigns to the dictionary self.cache_data
+            the item value for the key key
+        """
