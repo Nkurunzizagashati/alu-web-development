bae44e38413f9f0104416cf0484950dfbe8dfde0
Fabrice <f.nkurunziz@alustudent.com>
2024-05-18 09:54:38 +0200

    caching

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -9,46 +9,21 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """
-        class LIFOCache that inherits BaseCaching
-        class and implements hte LIFO caching system
-    """
-
-    def __init__(self):
-        """
-            an init method to inherit all the methods
-            from the class this LIFOCache class is inheriting
-            from
-        """
-        super().__init__()
-        self.order = []
+    """LIFO cache"""
 
     def put(self, key, item):
-        """
-            put method to put item into the cache.
-            It assigns to the dictionary self.cache_data
-            the item value for the key key
-        """
+        """Add an item in the cache"""
         if key is None or item is None:
             return
-
-        if key in self.cache_data:
-            self.order.remove(key)
-
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                discard = next(reversed(list(self.cache_data)))
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
         self.cache_data[key] = item
-        self.order.append(key)
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.order.pop()
-            del self.cache_data[last_key]
-            print(f"DISCARD: {last_key}")
 
     def get(self, key):
-        """
-            Return the value in self.cache_data linked to key.
-            If key is None or if the key does not exist in
-            self.cache_data, return None.
-        """
-        if key is None:
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
